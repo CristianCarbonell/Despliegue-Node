6d50d7ccf727236075b265d5795d659abba95300
CristianCarbonell <cristian2306c@gmail.com>
2024-05-26 02:40:35 +0200

    Añadido endpoint GET usuarios

--- Recetas/routes/usuarios.js
+++ Recetas/routes/usuarios.js
@@ -2,6 +2,17 @@ const express = require('express');
 let Usuario = require(__dirname + '/../models/Usuario');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Usuario.find().then((resultado) => {
+        res.status(200)
+            .send({ok: true,resultado:resultado})
+    }).catch((error) => {
+        res.status(400)
+            .send({ok: false,
+            error: "Error obteniendo usuarios"})
+    })
+})
+
 router.post('/',(req,res)=>{
     let nuevoUsuario = new Usuario({
         usuario: req.body.usuario,
